e12927d2001d85c549ac8c25c91a89ff23798cb7
Felipe González <felipe.gonzalezal.13@sansano.usm.cl>
2016-09-27 20:31:42 -0300

    Carga de datos de BD en el layout del detalle de oferta

--- app/src/main/java/techwork/ami/Need/NeedOffer/NeedOfferViewActivity.java
+++ app/src/main/java/techwork/ami/Need/NeedOffer/NeedOfferViewActivity.java
@@ -21,8 +21,7 @@ import techwork.ami.RequestHandler;
 
 public class NeedOfferViewActivity extends AppCompatActivity {
 
-    TextView tvTittle;
-    TextView tvPrice;
+    TextView tvTittle,tvPrice,tvCompany,tvDescription,tvDateIni,tvDateFin,tvStock,tvMaxPPerson;
     Button btnAccept;
     Button btnDiscard;
     String idOffer;
@@ -32,18 +31,32 @@ public class NeedOfferViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.need_offer_view_activity);
 
+        //Init textviews
         tvTittle = (TextView)findViewById(R.id.tv_need_offer_view_tittle);
         tvPrice = (TextView)findViewById(R.id.tv_need_offer_view_price);
+        tvCompany= (TextView)findViewById(R.id.tv_need_offer_view_company);
+        tvDescription= (TextView)findViewById(R.id.tv_need_offer_view_description);
+        tvDateIni = (TextView)findViewById(R.id.tv_need_offer_view_date_ini);
+        tvDateFin = (TextView)findViewById(R.id.tv_need_offer_view_date_fin);
+        //tvStock = (TextView)findViewById(R.id.tv_need_offer_view_stock);
+        tvMaxPPerson = (TextView)findViewById(R.id.tv_need_offer_view_max_person);
+
+        //Init buttons
         btnAccept = (Button)findViewById(R.id.btn_need_offer_view_accept);
         btnDiscard= (Button)findViewById(R.id.btn_need_offer_view_discard);
 
+        //Get info from NeedOfferActivity
         Bundle bundle = getIntent().getExtras();
 
         idOffer = bundle.getString(Config.TAG_GNO_IDOFFER);
         tvTittle.setText(bundle.getString(Config.TAG_GNO_TITTLE));
         tvPrice.setText("$"+String.format(Config.CLP_FORMAT,bundle.getInt(Config.TAG_GNO_PRICEOFFER)));
+        tvCompany.setText(bundle.getString(Config.TAG_GNO_COMPANY));
+        tvDateIni.setText("Fecha de publicación: "+bundle.getString(Config.TAG_GNO_DATEINI));
+        tvDateFin.setText("Fecha de expiración: "+bundle.getString(Config.TAG_GNO_DATEFIN));
+        tvMaxPPerson.setText("¡Puedes reservar hasta "+bundle.getString(Config.TAG_GNO_MAXPPERSON)+" unidades!");
 
-        //Gettin id of user
+        //Get id of user
         SharedPreferences sharePref= getSharedPreferences(Config.KEY_SHARED_PREF, Context.MODE_PRIVATE);
         final String id = sharePref.getString(Config.KEY_SP_ID,"-1");
 
@@ -106,13 +119,11 @@ public class NeedOfferViewActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 class discardNeedOfferAsyncTask extends AsyncTask<Void,Void,String>{
-                    private ProgressDialog loading;
+
                     @Override
                     protected void onPreExecute(){
                         super.onPreExecute();
-                        loading = ProgressDialog.show(NeedOfferViewActivity.this,
-                                getResources().getString(R.string.saving),
-                                getResources().getString(R.string.wait),false,false);
+                        Toast.makeText(getApplicationContext(),"Aceptando oferta..",Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
@@ -128,7 +139,6 @@ public class NeedOfferViewActivity extends AppCompatActivity {
                     @Override
                     protected void onPostExecute(String s){
                         super.onPostExecute(s);
-                        loading.dismiss();
                         if (s.equals("0")){
                             Toast.makeText(getApplicationContext(), "Oferta rechazada", Toast.LENGTH_SHORT).show();
                         }else{
