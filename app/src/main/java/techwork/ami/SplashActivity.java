23bc3acc266a012fe3e8975ae790d5cbf9d8d5be
Felipe González <felipe.gonzalezal.13@sansano.usm.cl>
2016-09-27 16:22:59 -0300

    Mas avances
    -Correción del splash
    -Correción logo splash.
    -Se agregó progresbar (simula la carga)
    -La APP permite instalarse en la SD

--- app/src/main/java/techwork/ami/SplashActivity.java
+++ app/src/main/java/techwork/ami/SplashActivity.java
@@ -1,17 +1,42 @@
 package techwork.ami;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+
+    ProgressBar progressBarHorizontal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        progressBarHorizontal = (ProgressBar)findViewById(R.id.progressBarSplash);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                for (int progress=0;progress<=100;progress+=1){
+                    try{
+                        Thread.sleep(30);
+                        progressBarHorizontal.setProgress(progress);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                startActivity(intent);
+                finish();
+            }
+        }).start();
+
+
+
     }
 }
