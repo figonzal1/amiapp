071d1b523baeb0378691192f3d38e3ba5f9e2451
Daniel Quinteros <daniel.quinteros.12@sansano.usm.cl>
2016-09-28 22:28:38 -0300

    Eliminación de archivos no usados.

--- app/src/main/java/techwork/ami/MainActivity.java
+++ app/src/main/java/techwork/ami/MainActivity.java
@@ -23,7 +23,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import techwork.ami.Filter.FilterActivity;
 import techwork.ami.ReservationsOffers.MyReservationsOffersActivity;
 
 public class MainActivity extends AppCompatActivity
@@ -200,8 +199,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_my_preferences) {
 
         } else if (id == R.id.nav_reserve_needs) {
-            Intent iFilterActivity= new Intent(MainActivity.this, FilterActivity.class);
-            startActivity(iFilterActivity);
+
         } else if (id == R.id.nav_reservations) {
             Intent iMyReservations= new Intent(MainActivity.this, MyReservationsOffersActivity.class);
             startActivity(iMyReservations);
