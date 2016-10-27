13805c0088ad31bb651354c0f9c624dfaac0ef99
Felipe González <felipe.gonzalezal.13@sansano.usm.cl>
2016-10-27 10:41:46 -0300

    Toolbar agregada a detalle de oferta. Correciones de layout

--- app/src/main/java/techwork/ami/Need/ListOfferCompanies/NeedOfferActivity.java
+++ app/src/main/java/techwork/ami/Need/ListOfferCompanies/NeedOfferActivity.java
@@ -67,7 +67,6 @@ public class NeedOfferActivity extends AppCompatActivity {
         });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         getNeedOffers();
     }
 
--- app/src/main/java/techwork/ami/Need/NeedOfferDetails/NeedOfferViewActivity.java
+++ app/src/main/java/techwork/ami/Need/NeedOfferDetails/NeedOfferViewActivity.java
@@ -6,10 +6,12 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -43,6 +45,9 @@ public class NeedOfferViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.need_offer_view_activity);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
         //Init textviews
@@ -202,7 +207,6 @@ public class NeedOfferViewActivity extends AppCompatActivity {
                 go.execute();
             }
         });
-
         getNeedOfferProducts();
 
     }
