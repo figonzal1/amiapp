46bb0ef54862c3a853a1e187b3b17041d3260a6d
Daniel Quinteros <daniel.quinteros.12@sansano.usm.cl>
2016-11-03 04:32:38 -0300

    Vibración al editar mi perfil.

--- app/src/main/java/techwork/ami/EditProfileActivity.java
+++ app/src/main/java/techwork/ami/EditProfileActivity.java
@@ -12,6 +12,7 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -548,7 +549,8 @@ public class EditProfileActivity extends AppCompatActivity {
 					editor.putString(Config.KEY_SP_EMAIL, email);
 					editor.putString(Config.KEY_SP_GENDER, idGender);
 					editor.apply();
-
+					Vibrator c = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+					c.vibrate(500);
 					Toast.makeText(EditProfileActivity.this, R.string.saveOk, Toast.LENGTH_LONG).show();
 					finish();
 				}
