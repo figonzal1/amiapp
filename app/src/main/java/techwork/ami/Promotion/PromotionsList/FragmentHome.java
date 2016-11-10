325d8f781ba6b3e3796f0726106adedbefe327bd
Daniel Quinteros <daniel.quinteros.12@sansano.usm.cl>
2016-11-10 02:15:15 -0300

    Nada.

--- app/src/main/java/techwork/ami/Promotion/PromotionsList/FragmentHome.java
+++ app/src/main/java/techwork/ami/Promotion/PromotionsList/FragmentHome.java
@@ -15,6 +15,7 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.NotificationCompat;
